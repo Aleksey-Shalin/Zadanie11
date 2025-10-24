11e84928cecb76e1fe5072d7c7bd367c3f2e46ab
Aleksey <aleksey_ktotamych@mail.ru>
2025-10-24 07:25:10 +0300

    Add files via upload

---
+++ Task11-1_Greeting.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<windows.h>
+
+void fullname_input(std::string* name, std::string* surname);    // прототип функции для ввода и вывода фамилии
+std::string greeting_output(std::string& name, std::string& surname);    // прототип функции для вывода приветствия
+
+// основная функция программы
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	std::string user_name;
+	std::string user_surname;
+	fullname_input(&user_name, &user_surname);
+	std::cout << greeting_output(user_name, user_surname);
+	return 0;
+}
+
+// реализация функции для ввода и вывода фамилии
+void fullname_input(std::string* name, std::string* surname)
+{
+	std::cout << "Введите ваше имя: ";
+	std::cin >> *name;
+	std::cout << "Введите вашу фамилию: ";
+	std::cin >> *surname;
+}
+
+// реализация функции для вывода приветствия
+std::string greeting_output(std::string& name, std::string& surname)
+{
+	std::string fullname = name + " " + surname;
+	return "Здравствуйте, " + fullname + "!";
+}
---
+++ Task11-2_Guessing.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<string>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Какая мышь воет как волк?" << std::endl;
+	std::string answer = "кузнечиковая";
+	std::string version;
+	std::cout << "Угадайте слово: ";
+	std::cin >> version;
+	int correct = answer.compare(version);
+	if (correct == 0)
+	{
+		std::cout << "Правильно, вы победили! Загаданное слово " << answer;
+	}
+	else if (correct !=0)
+		do
+		{
+			std::cout << "Угадайте слово: ";
+			std::cin >> version;
+			if (version.length() > answer.length())
+			{
+				std::cout << "Ваше слово длиннее загаднного" << std::endl;
+			}
+			else if (version.length() < answer.length())
+			{
+				std::cout << "Ваше слово короче загаданного" << std::endl;
+			}
+			else { std::cout << "Вы угадали длину слова" << std::endl; }
+		} while (true);
+
+
+	/* version 2
+	while (version != answer)
+	{
+		std::cout << "Угадайте слово: ";
+		std::cin >> version;
+		if (version.length() > answer.length())
+		{
+			std::cout << "Ваше слово длиннее загаднного" << std::endl;
+		}
+		else if (version.length() < answer.length())
+		{
+			std::cout << "Ваше слово короче загаданного" << std::endl;
+		}
+		else { std::cout << "Вы угадали длину слова" << std::endl; }
+	}
+	std::cout << "Правильно, вы победили! Загаданное слово " << answer;*/
+	return 0;
+}
