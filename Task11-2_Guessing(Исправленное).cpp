ef1f58a74a311ccf9009e394d6f37362f5d2db6c
Aleksey <aleksey_ktotamych@mail.ru>
2025-10-27 09:32:14 +0300

    Add files via upload

---
+++ Task11-2_Guessing(Исправленное).cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+#include<windows.h>
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Какая мышь воет как волк?" << std::endl;
+	std::string answer{ "кузнечиковая" };
+	std::string version;
+	do
+	{
+		std::cout << "Угадайте слово: ";
+		std::cin >> version;
+		if (version == answer)
+		{
+			std::cout << "Правильно, вы победили! Загаданное слово " << answer << std::endl;
+			break;
+		}
+
+		if (version.length() > answer.length())
+		{
+			std::cout << "Ваше слово длиннее загаднного" << std::endl;
+		}
+		else if (version.length() < answer.length())
+		{
+			std::cout << "Ваше слово короче загаданного" << std::endl;
+		}
+		else { std::cout << "Вы угадали длину слова" << std::endl; }
+	} while (true);
+	return 0;
+}
